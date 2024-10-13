4a85f2052947ebc683fa54020e746bfcd5a7b924
stormfather0 <ivan.ystskovyna@gmail.com>
2024-10-13 14:10:58 +0300

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,7 +82,18 @@ matchingItem = item;
             quantity: 1,
            })
        }
-       console.log(cart);
+
+       let cartQuantity = 0;
+       cart.forEach((item) =>{
+            cartQuantity += item.quantity;
+       });
+
+       document.querySelector('.js-cart-quantity')
+       .innerHTML = cartQuantity;
+
+
+    
+       
        
     }); 
 });
