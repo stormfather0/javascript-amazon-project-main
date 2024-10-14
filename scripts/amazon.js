075a251ed1749a18a6e032c4cc4ccf2fbcd80e63
stormfather0 <ivan.ystskovyna@gmail.com>
2024-10-14 10:39:23 +0300

    Move addToCart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,146 +1,81 @@
-import {cart} from '../data/cart.js';
-// to rename variable: import {cart as myCart}
-import {products} from '../data/products.js'
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
-let productsHTML = ``;
+
+let productsHTML = '';
 
 products.forEach((product) => {
-productsHTML += `
-            <div class="product-container">
-            <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
-            </div>
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-            <div class="product-name limit-text-to-2-lines">
-                ${product.name}
-            </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-            <div class="product-rating-container">
-                <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
-                <div class="product-rating-count link-primary">
-                ${product.rating.count}
-                </div>
-            </div>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
 
-            <div class="product-price">
-                $${(product.priceCents / 100).toFixed(2)}
-            </div>
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
 
-            <div class="product-quantity-container">
-                <select class="js-quantity-selector-${product.id}">
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                </select>
-            </div>
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
 
-            <div class="product-spacer"></div>
+      <div class="product-spacer"></div>
 
-            <div class="added-to-cart js-added-to-cart-${product.id}">
-                <img src="images/icons/checkmark.png ">
-                Added
-            </div>
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-id="${product.id}">
-                Add to Cart
-            </button>
-            </div>
-`;
-
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+    let cartQuantity = 0;
 
-document.querySelectorAll('.js-add-to-cart')
-.forEach((button) => {
-    button.addEventListener('click', () => {
-      
-       const productId = button.dataset.productId;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
 
-      
-       let selectedQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`)
-       .value)
-
-       
-     
-     
-       
-       
-    //    let timeoutAddedToCartNotification = setTimeout(() => {
-    //     document.querySelector(`.js-added-to-cart-${productId}`).style.opacity = 0;
-    //    }
-    //     , 2000)
-
-    const addedMessageTimeouts = {};
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+}
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-
-      addedMessage.classList.add('added-to-cart-visible');
-    })});
-
-
-       
-
-
-let matchingItem;
-
-       cart.forEach((item) => {
-        if (productId === item.productId) {
-matchingItem = item;
-        }
-       });
-
-       if (matchingItem) {
-        matchingItem.quantity += selectedQuantity;
-       } else {
-        cart.push({
-            productId: productId,
-            quantity: selectedQuantity,
-           })
-       }
-
-
-
-
-       let cartQuantity = 0;
-       cart.forEach((item) =>{
-            cartQuantity += item.quantity;
-           
-       });
-
-       document.querySelector('.js-cart-quantity')
-       .innerHTML = cartQuantity;
-
-
-    
-   
-       
-    
-    
-
-       
-       
-    
-       
-    }); 
-
-    
-})
-
-
-
-   
-
+      const productId = button.dataset.productId;
+addToCart(productId);
+updateCartQuantity();
+    });
+  });
