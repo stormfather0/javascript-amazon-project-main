3790ffbe9f1fa5c34184c325be7f59dec85c2499
stormfather0 <ivan.ystskovyna@gmail.com>
2024-10-14 17:23:57 +0300

    delete products after clicking delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -21,7 +21,7 @@ cartSummaryHTML +=
 `
 
 
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -112,6 +112,7 @@ document.querySelectorAll('.js-delete-link')
       console.log(cart);
       
       
-        
+      const container =  document.querySelector(`.js-cart-item-container-${productId}`);
+       container.remove();
     })
 })
